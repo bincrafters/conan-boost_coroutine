21315c750fc7bd4ca2b4b19ad5f4ab37817b12d1
Jerry W <jerrywiltse@gmail.com>
2018-01-25 01:12:49 -0500

    Refresh round 2

--- conanfile.py
+++ conanfile.py
@@ -8,7 +8,8 @@ class BoostCoroutineConan(ConanFile):
     name = "boost_coroutine"
     version = "1.66.0"
     url = "https://github.com/bincrafters/conan-boost_coroutine"
-
+    author = "Bincrafters <bincrafters@gmail.com>"
+    exports = ["LICENSE.md"]
     lib_short_names = ["coroutine"]
     is_header_only = False
 
